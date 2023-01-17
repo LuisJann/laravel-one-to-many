0ad038308c31431bff27619ae6bf33e8676192c2
LuisJann <giannoneluigi10@gmail.com>
2023-01-17 10:28:20 +0100

    Add types table

--- database/migrations/2023_01_16_170234_add_foreing_type_type_id_to_projects_table.php
+++ database/migrations/2023_01_16_170234_add_foreing_type_type_id_to_projects_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('projects', function (Blueprint $table) {
             $table->unsignedBigInteger('type_id')->nullable()->after('id');
-            $table->foreign('project_id')->references('id')->on('types');
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('set null');
         });
     }
 
@@ -28,6 +28,7 @@ return new class extends Migration
     {
         Schema::table('projects', function (Blueprint $table) {
             $table->dropForeign('projects_type_id_foreign');
+            $table->dropColumn('type_id');
         });
     }
 };
